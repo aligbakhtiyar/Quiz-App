b389723ed631ebb79930b44ce23a278960b6b559
aligbakhtiyar <ahmadbakhtiyar@gmail.com>
2023-02-23 02:26:25 +0530

    components

---
+++ quizapp/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import QuizScreen from "./components/QuizScreen";
+import JoinScreen from "./components/JoinScreen";
+
+import React from "react";
+import Navbar from "./components/Navbar";
+
+function App() {
+  const [isQuizStarted, setIsQuizStarted] = useState(false);
+  return (
+    <>
+      <Navbar />
+      <div className="container-sm">
+        {isQuizStarted ? (
+          <QuizScreen retry={() => setIsQuizStarted(false)} />
+        ) : (
+          <JoinScreen start={() => setIsQuizStarted(true)} />
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ quizapp/src/components/JoinScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function JoinScreen({start}) {
+  return (
+    <div>
+      <h2>Join Quiz</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae ea quam
+        temporibus fugiat perspiciatis, alias laudantium sit reiciendis,
+        similique dignissimos consectetur deleniti, saepe ab deserunt at nemo
+        ipsam est ex.
+      </p>
+      <button onClick={start}>Start</button>
+    </div>
+  );
+}
+
+export default JoinScreen;
